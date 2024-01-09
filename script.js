a75e4a69c9f7361a5b8aaa18a78f53123b5eea0b
Nick Cook <118715474+Nickc009@users.noreply.github.com>
2024-01-09 22:35:08 +0000

    A lot has changed.

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".zoomImage").mouseover(function(){
-        $(this).animate({height: 300, width: 300}, "slow");
-    });
-
-    $(".zoomImage").mouseout(function(){
-        $(this).animate({height: 150, width: 150}, "slow");
-    });
-});
